b97372f4068e3f4af426fe83eb139a3ce0090fe5
rqwangXD <81528144+rqwangXD@users.noreply.github.com>
2022-07-18 01:16:36 -0700

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api
+from eth_account.messages import encode_defunct
 import json
 import eth_account
 import algosdk
@@ -14,7 +15,8 @@ def verify():
     #Check if signature is valid
     result = False #Should only be true if signature validates
     if content['payload']['platform'] == 'Ethereum':
-        if eth_account.Account.recover_message(json.dumps(content['payload']),signature=content['sig']) == content['payload']['pk']:
+        signable_message = encode_defunct(text=json.dumps(content['payload']))
+        if eth_account.Account.recover_message(signable_message, signature=content['sig']) == content['payload']['pk']:
             result = True
     elif content['payload']['platform'] == 'Algorand':
         if algosdk.util.verify_bytes(json.dumps(content['payload']).encode('utf-8'),content['sig'],content['payload']['pk']):
