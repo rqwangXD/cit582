bd1eeb1a2cf8eb32277179f184f75b865c98049f
rqwangXD <81528144+rqwangXD@users.noreply.github.com>
2022-07-18 01:12:04 -0700

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -14,9 +14,11 @@ def verify():
     #Check if signature is valid
     result = False #Should only be true if signature validates
     if content['payload']['platform'] == 'Ethereum':
-        result = (eth_account.Account.recover_message(json.dumps(content['payload']),signature=content['sig'].hex()) == content['payload']['pk'])
+        if eth_account.Account.recover_message(json.dumps(content['payload']),signature=content['sig']) == content['payload']['pk']:
+            result = True
     elif content['payload']['platform'] == 'Algorand':
-        result = algosdk.util.verify_bytes(json.dumps(content['payload']).encode('utf-8'),content['sig'],content['payload']['pk'])
+        if algosdk.util.verify_bytes(json.dumps(content['payload']).encode('utf-8'),content['sig'],content['payload']['pk']):
+            result = True
     return jsonify(result)
 
 if __name__ == '__main__':
